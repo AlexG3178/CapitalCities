f394cac18bf4ce834b6f336ebab75fcafb7aa8bb
alexg3178 <alexg3178@gmail.com>
2019-03-04 20:26:26 +0200

    Initial Commit

---
+++ CapitalCities/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Alexandr Grigoriev on 3/4/19.
+//  Copyright © 2019 AlexG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
